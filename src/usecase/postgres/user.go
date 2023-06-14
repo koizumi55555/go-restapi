53fd77d407fb6728e3c39c83498f1d8035b15eb3
koizumi55555 <136076504+koizumi55555@users.noreply.github.com>
2023-06-14 13:34:03 +0900

    Update user.go

--- src/usecase/postgres/user.go
+++ src/usecase/postgres/user.go
@@ -11,26 +11,26 @@ import (
 // ユーザ情報の取得
 func (p Postgres) GetUserDB(id string) (user entitiy.User, err error) {
 
-	user1 := Users{}
-	result := p.Conn.Find(&user1, "id = ?", id)
+	resultUser := Users{}
+	getResult := p.Conn.Find(&resultUser, "id = ?", id)
 
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return entitiy.User{}, result.Error
+	if errors.Is(getResult.Error, gorm.ErrRecordNotFound) {
+		return entitiy.User{}, getResult.Error
 	}
 
 	return entitiy.User{
-		ID:    user1.ID,
-		Name:  user1.UserName,
-		Email: user1.Email,
+		ID:    resultUser.ID,
+		Name:  resultUser.UserName,
+		Email: resultUser.Email,
 	}, nil
 }
 
 // ユーザの削除
 func (p Postgres) DeleteUserDB(id string) (err error) {
-	result := p.Conn.Where("id = ?", id).Delete(&entitiy.User{})
+	deleteResult := p.Conn.Where("id = ?", id).Delete(&entitiy.User{})
 
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return result.Error
+	if errors.Is(deleteResult.Error, gorm.ErrRecordNotFound) {
+		return deleteResult.Error
 	}
 	return nil
 }
@@ -38,70 +38,73 @@ func (p Postgres) DeleteUserDB(id string) (err error) {
 // ユーザの更新
 func (p Postgres) UpdateUserDB(updateUser entitiy.User) (user entitiy.User, err error) {
 
-	user1 := Users{}
-	result1 := p.Conn.Find(&user1, "id = ?", updateUser.ID)
+	resultUser := Users{}
+	getResult := p.Conn.Find(&resultUser, "id = ?", updateUser.ID)
 
-	if errors.Is(result1.Error, gorm.ErrRecordNotFound) {
-		return entitiy.User{}, result1.Error
+	if errors.Is(getResult.Error, gorm.ErrRecordNotFound) {
+		return entitiy.User{}, getResult.Error
 	}
 
-	user1.UserName = updateUser.Name
-	user1.Email = updateUser.Email
-	result2 := p.Conn.Save(&user1)
+	resultUser.UserName = updateUser.Name
+	resultUser.Email = updateUser.Email
+	updateResult := p.Conn.Save(&resultUser)
 
-	if errors.Is(result2.Error, gorm.ErrRecordNotFound) {
-		return entitiy.User{}, result2.Error
+	if errors.Is(updateResult.Error, gorm.ErrRecordNotFound) {
+		return entitiy.User{}, updateResult.Error
 	}
 
 	return entitiy.User{
-		ID:    user1.ID,
-		Name:  user1.UserName,
-		Email: user1.Email,
+		ID:    resultUser.ID,
+		Name:  resultUser.UserName,
+		Email: resultUser.Email,
 	}, nil
 }
 
 // ユーザの登録
 func (p Postgres) CreateUserDB(createUser entitiy.User) (user entitiy.User, err error) {
 
-	user1 := Users{}
-	result1 := p.Conn.Order("id desc").First(&user1)
+	resultUser := Users{}
+	getResult := p.Conn.Order("id desc").First(&resultUser)
 
-	if errors.Is(result1.Error, gorm.ErrRecordNotFound) {
-		return entitiy.User{}, result1.Error
+	if errors.Is(getResult.Error, gorm.ErrRecordNotFound) {
+		return entitiy.User{}, getResult.Error
 	}
 
-	user2 := Users{}
-	i, _ := strconv.Atoi(user1.ID)
-	user2.ID = strconv.Itoa(i + 1)
-	user2.UserName = createUser.Name
-	user2.Email = createUser.Email
-	result2 := p.Conn.Save(&user2)
+	i, _ := strconv.Atoi(resultUser.ID)
 
-	if errors.Is(result2.Error, gorm.ErrRecordNotFound) {
-		return entitiy.User{}, result2.Error
+	newUser := Users{
+		ID:       strconv.Itoa(i + 1),
+		UserName: createUser.Name,
+		Email:    createUser.Email,
+	}
+
+	createResult := p.Conn.Save(&newUser)
+
+	if errors.Is(createResult.Error, gorm.ErrRecordNotFound) {
+		return entitiy.User{}, createResult.Error
 	}
 
 	return entitiy.User{
-		ID:    user2.ID,
-		Name:  user2.UserName,
-		Email: user2.Email,
+		ID:    newUser.ID,
+		Name:  newUser.UserName,
+		Email: newUser.Email,
 	}, nil
 }
 
 // ユーザ一覧取得
-func (p Postgres) GetListUsersDB() (user []entitiy.User, err error) {
-	user1 := []Users{}
-	result := p.Conn.Order("id asc").Find(&user1)
+func (p Postgres) ListUsersDB() (user []entitiy.User, err error) {
+	resultUser := []Users{}
+	getResult := p.Conn.Order("id asc").Find(&resultUser)
 
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return nil, result.Error
+	if errors.Is(getResult.Error, gorm.ErrRecordNotFound) {
+		return nil, getResult.Error
 	}
 
-	for i := 0; i < len(user1); i++ {
+	for i := 0; i < len(resultUser); i++ {
 		user = append(user, entitiy.User{
-			ID:    user1[i].ID,
-			Name:  user1[i].UserName,
-			Email: user1[i].Email,
+			ID:    resultUser[i].ID,
+			Name:  resultUser[i].UserName,
+			Email: resultUser[i].Email,
 		})
 	}
 
