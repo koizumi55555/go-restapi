56b851deb5e8e8fdb79e759fd6cfdca05586be9a
koizumi55555 <136076504+koizumi55555@users.noreply.github.com>
2023-06-16 12:23:40 +0900

    Update interface.go

--- src/usecase/interface.go
+++ src/usecase/interface.go
@@ -5,7 +5,7 @@ import "koizumi55555/go-restapi/src/entitiy"
 type PostgresIf interface {
 	GetUserDB(id string) (user entitiy.User, err error)
 	DeleteUserDB(id string) (err error)
-	UpdateUserDB(updateUser entitiy.User) (user entitiy.User, err error)
+	UpdateUserDB(getUser entitiy.User, updateUser entitiy.User) (user entitiy.User, err error)
 	CreateUserDB(createUser entitiy.User) (user entitiy.User, err error)
 	ListUsersDB() (user []entitiy.User, err error)
 }
